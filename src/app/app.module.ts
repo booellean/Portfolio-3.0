dc00a99fe37f2e48d5fe13c1334fc06056beafa5
booellean <booelle.commissions@gmail.com>
2018-05-29 09:20:36 -0700

    finalized routing paths.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { WebdevComponent } from './webdev/webdev.component';
+import { DesignComponent } from './design/design.component';
+import { IllustrationComponent } from './illustration/illustration.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    WebdevComponent,
+    DesignComponent,
+    IllustrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
