b4cd6104723fb77f62e899c1e6f910b1e0893e61
booellean <booelle.commissions@gmail.com>
2018-05-29 18:32:56 -0700

    updated Gruntfile to include npmTasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,60 @@
 grunt.loadNpmTasks('grunt-responsive-images');
 
-module.exports = function(grunt) {
-    grunt.initConfig({
-        responsive_images: {
-          dev: {
-            options: {
-              sizes: [{
-                name: 'x-small',
-                width: 200,
-                suffix: "-200",
-                quality: 85
-              },{
-                name: 'small',
-                width: 400,
-                suffix: "-400",
-                quality: 85
-              },{
-                name: 'med',
-                width: 600,
-                suffix: "-600",
-                quality: 85
-              },{
-                name: "large",
-                width: 800,
-                suffix: "-800",
-                quality: 85
-              },{
-                name: "x-large",
-                width: 1100,
-                quality: 85
-              }]
-            },
-            files: [{
-              expand: true,
-              src: ['*.{jpg,gif,png}'],
-              cwd: 'src/assets/images/design/',
-              dest: 'src/assets/images/design-RAW/'
-            },{
-              expand: true,
-              src: ['*.{jpg,gif,png}'],
-              cwd: 'src/assets/images/illustration/',
-              dest: 'src/assets/images/illustration-RAW/'
-            },{
-              expand: true,
-              src: ['*.{jpg,gif,png}'],
-              cwd: 'src/assets/images/webdev/',
-              dest: 'src/assets/images/webdev-RAW/'
-            }]
-          }
+export default function (grunt) {
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          sizes: [{
+            name: 'x-small',
+            width: 200,
+            suffix: "-200",
+            quality: 85
+          }, {
+            name: 'small',
+            width: 400,
+            suffix: "-400",
+            quality: 85
+          }, {
+            name: 'med',
+            width: 600,
+            suffix: "-600",
+            quality: 85
+          }, {
+            name: "large",
+            width: 800,
+            suffix: "-800",
+            quality: 85
+          }, {
+            name: "x-large",
+            width: 1100,
+            quality: 85
+          }]
         },
-            /* Clear out the images directory if it exists */
+        files: [{
+          expand: true,
+          src: ['*.{jpg,gif,png}'],
+          cwd: 'src/assets/images/design/',
+          dest: 'src/assets/images/design-RAW/'
+        }, {
+          expand: true,
+          src: ['*.{jpg,gif,png}'],
+          cwd: 'src/assets/images/illustration/',
+          dest: 'src/assets/images/illustration-RAW/'
+        }, {
+          expand: true,
+          src: ['*.{jpg,gif,png}'],
+          cwd: 'src/assets/images/webdev/',
+          dest: 'src/assets/images/webdev-RAW/'
+        }]
+      }
+    },
+    /* Clear out the images directory if it exists */
     clean: {
       dev: {
-        src: ['src/assets/images/design/', 'src/assets/images/illustration/', 'src/assets/images/webdev/'],
+        src: ['src/assets/images/design/', 'src/assets/images/illustration/', 'src/assets/images/webdev/']
       },
     },
-
     /* Generate the images directory if it is missing */
     mkdir: {
       dev: {
@@ -64,16 +63,21 @@ module.exports = function(grunt) {
         },
       },
     },
-
     /* Copy the "fixed" images that don't go through processing into the images/directory */
     copy: {
       dev: {
         files: [{
           expand: true,
           src: ['src/assets/images/design/*.{gif,jpg,png}', 'src/assets/images/illustration/*.{gif,jpg,png}', 'src/assets/images/webdev/*.{gif,jpg,png}'],
-          dest: ['src/assets/images/design/', 'src/assets/images/illustration/', 'src/assets/images/webdev/'],
+          dest: ['src/assets/images/design/', 'src/assets/images/illustration/', 'src/assets/images/webdev/']
         }]
       },
     },
-      })
-}
+  });
+  
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+};
