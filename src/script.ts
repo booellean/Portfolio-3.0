76da57fa2a03fd1c2a773d8c659894516a2b9bec
booellean <booelle.commissions@gmail.com>
2018-06-03 13:22:13 -0700

    added new function headerHeight that keeps the header contained with the footer and header are both present on the load page. Modified the headerSkillsPos function to scroll properly during that condition.

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,22 @@
 // Global DOM behaviors, separate from all dynamically generated content behaviors
 
+window.addEventListener('load', headerHeight);
 window.addEventListener('scroll', headerSkillsPos);
 window.addEventListener('resize', headerSkillsPos);
+
+function headerHeight(){
+  let sPos = document.getElementById('skills');
+  let hMainH = document.getElementById('page-header').clientHeight;
+  let pCHeight = document.getElementById('portfolio-content').clientHeight;
+  let bodHeight = hMainH + pCHeight;
+  let winHeight = window.innerHeight;
+
+  if(winHeight > bodHeight){
+    sPos.style.height = `${pCHeight}px`;
+  }else{
+    return;
+  }
+}
 
 function headerSkillsPos() {
   let sPos = document.getElementById('skills');
@@ -12,6 +27,10 @@ function headerSkillsPos() {
   let fMainH = document.getElementById('page-footer').clientHeight;
   let docH = document.body.scrollHeight - document.body.clientHeight;
   let docDiff = docH-fMainH;
+  //Used to see if page-footer is viewable on load. Will affect scrolling later
+  let pCHeight = document.getElementById('portfolio-content').clientHeight;
+  let bodHeight = hMainH + pCHeight;
+  let winHeight = window.innerHeight;
 
   if(yPos<hMainH){
     //will keep the top position just under the header
@@ -23,9 +42,15 @@ function headerSkillsPos() {
     sPos.style.top = `0px`;
   }
   if(yPos >=hMainH && yPos>=docDiff){
-    //footer is viewable, will keep top at negative value of yoffset of footer
-    let diff = docDiff - yPos;
-    sPos.style.top =`${diff}px`;
+    if(winHeight > bodHeight){
+      let diff = hMainH - yPos;
+    sPos.style.top = `${diff}px`;
+    }else{
+      //footer is viewable, will keep top at negative value of yoffset of footer
+      let diff = docDiff - yPos;
+      sPos.style.top =`${diff}px`;
+    }
   }
+  console.log(yPos);
 
 }
